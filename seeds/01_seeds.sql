6b7c9e99d1c0e9642dca05d978d255d740bf735f
cindyhalim <jcindyhalim@gmail.com>
2019-09-17 22:03:22 +0000

    Add data to tables

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,15 @@
+INSERT INTO users VALUES (1, 'Eva Stanley', 'ebastianguerra@ymail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+INSERT INTO users VALUES (2, 'Louisa Meyer', 'jacksonrose@hotmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+INSERT INTO users VALUES (3, 'Dominic Parks', 'victoriablackwell@outlook.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+
+INSERT INTO properties VALUES (1, 2, 'Speed Lamp', 'description', 'https://images.pexels.com/photos/2086676/pexels_photo.jpg', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg', 930.61, '536 Namsub Highway', 6, 5, 8, 'Canada',  'Sotboske', 'Quebec', '28142');
+INSERT INTO properties VALUES (2, 3, 'Blank Corner', 'description', 'https://images.pexels.com/photos/2121121/pexels.jpg', 'https://images.pexels.com/photos/alskjdas.jpg', 85234, '651 Nami Road', 6, 6, 7, 'Canada', 'Bohbatev', 'Alberta', '83680');
+INSERT INTO properties VALUES (3, 1, 'Habit Mix', 'description', 'https://images.pexels.com/aksljdas.jpg', 'https://images.pexels.com/aksljdlaksjd.jpg', 300, '951 Dovercourt Rd', 1, 1, 1, 'Canada', 'Toronto', 'Ontario', 'M42 IC5');
+
+INSERT INTO reservations VALUES (1, '2018-09-11', '2018-09-26', 1, 1);
+INSERT INTO reservations VALUES (2, '2019-01-04', '2019-02-01', 2, 2);
+INSERT INTO reservations VALUES (3, '2021-10-01', '2021-10-14', 3, 3);
+
+INSERT INTO property_reviews VALUES (1, 2, 2, 2, 3, 'message');
+INSERT INTO property_reviews VALUES (2, 1, 2, 1, 5, 'message');
+INSERT INTO property_reviews VALUES (3, 2, 1, 3, 3, 'message');
