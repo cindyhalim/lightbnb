6607dc31833ed3994cfb8078f2d556addd741e05
cindyhalim <jcindyhalim@gmail.com>
2019-09-18 19:15:18 +0000

    Add tables as per stretch work, add web app folder and modify database.js to accept SQL queries

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -1,5 +1,15 @@
+const { Pool } = require('pg');
 const properties = require('./json/properties.json');
 const users = require('./json/users.json');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'lightbnb'
+  // port: '3000'
+});
+
 
 /// Users
 
@@ -9,16 +19,22 @@ const users = require('./json/users.json');
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function(email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
-    } else {
-      user = null;
-    }
-  }
-  return Promise.resolve(user);
+  const queryString = `SELECT * FROM users WHERE email = $1;`
+  const values = [email];
+  return pool.query(queryString, values)
+  .then(res => res.rows[0])
+  .catch(err => console.log('query error', err.stack))
+
+  // let user;
+  // for (const userId in users) {
+  //   user = users[userId];
+  //   if (user.email.toLowerCase() === email.toLowerCase()) {
+  //     break;
+  //   } else {
+  //     user = null;
+  //   }
+  // }
+  // return Promise.resolve(user);
 }
 exports.getUserWithEmail = getUserWithEmail;
 
@@ -28,7 +44,11 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+  const queryString = `SELECT * FROM users WHERE id = $1`
+  const values = [id]
+  return pool.query(queryString, values)
+  .then(res => res.rows[0])
+  .catch(err => console.log('query error', err.stack))
 }
 exports.getUserWithId = getUserWithId;
 
@@ -39,10 +59,14 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  // const userId = Object.keys(users).length + 1;
+  // user.id = userId;
+  // users[userId] = user;
+  const values = [user.name, user.email, user.password]
+  const queryString = `INSERT INTO users(name, email, password) VALUES ($1, $2, $3) RETURNING *`
+  return pool.query(queryString, values)
+  .then(res => res.rows[0])
+  .catch(err => console.log('query error', err.stack))
 }
 exports.addUser = addUser;
 
@@ -66,14 +90,22 @@ exports.getAllReservations = getAllReservations;
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
+
+
 const getAllProperties = function(options, limit = 10) {
-  const limitedProperties = {};
-  for (let i = 1; i <= limit; i++) {
-    limitedProperties[i] = properties[i];
-  }
-  return Promise.resolve(limitedProperties);
-}
+  const queryString = `SELECT * FROM properties LIMIT $1;`;
+  return pool.query(queryString, [limit])
+  .then(res => res.rows)
+  .catch ( err => console.log('query error', err.stack))
+  
+  // const limitedProperties = {};
+  // for (let i = 1; i <= limit; i++) {
+  //   limitedProperties[i] = properties[i];
+  // }
+  // return Promise.resolve(limitedProperties);
+};
 exports.getAllProperties = getAllProperties;
+
 
 
 /**
--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -49,3 +49,19 @@ CREATE TABLE property_reviews (
   message TEXT
 );
 
+CREATE TABLE rates (
+  id SERIAL PRIMARY KEY NOT NULL,
+  start_date TIMESTAMP NOT NULL,
+  end_date TIMESTAMP NOT NULL,
+  cost_per_night INTEGER NOT NULL,
+  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE
+)
+
+CREATE TABLE guest_reviews (
+  id SERIAL PRIMARY KEY NOT NULL,
+  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
+  rating SMALLINT NOT NULL DEFAULT 0,
+  message TEXT
+)
