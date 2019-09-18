72954216eaa45cd7e140755ec9c28e5f2a25ba0d
cindyhalim <jcindyhalim@gmail.com>
2019-09-18 22:00:38 +0000

    Modify addProperty function to accept SQL query

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -7,7 +7,6 @@ const pool = new Pool({
   password: '123',
   host: 'localhost',
   database: 'lightbnb'
-  // port: '3000'
 });
 
 
@@ -24,17 +23,6 @@ const getUserWithEmail = function (email) {
   return pool.query(queryString, values)
     .then(res => res.rows[0])
     .catch(err => console.log('query error', err.stack))
-
-  // let user;
-  // for (const userId in users) {
-  //   user = users[userId];
-  //   if (user.email.toLowerCase() === email.toLowerCase()) {
-  //     break;
-  //   } else {
-  //     user = null;
-  //   }
-  // }
-  // return Promise.resolve(user);
 }
 exports.getUserWithEmail = getUserWithEmail;
 
@@ -64,9 +52,6 @@ const addUser = function (user) {
   return pool.query(queryString, values)
   .then(res => res.rows[0])
   .catch(err => console.log('query error', err.stack))
-  // const userId = Object.keys(users).length + 1;
-  // user.id = userId;
-  // users[userId] = user;
 }
 exports.addUser = addUser;
 
@@ -90,7 +75,6 @@ LIMIT $2`;
   return pool.query(queryString, values)
     .then(res => res.rows)
     .catch(err => console.log('query error', err.stack))
-  // return getAllProperties(null, 2);
 }
 exports.getAllReservations = getAllReservations;
 
@@ -105,16 +89,13 @@ exports.getAllReservations = getAllReservations;
 
 
 const getAllProperties = function (options, limit = 10) {
-    // 1
     const queryParams = [];
-    // 2
     let queryString = `
     SELECT properties.*, avg(property_reviews.rating) as average_rating
     FROM properties
     JOIN property_reviews ON properties.id = property_id
     `;
-  
-    // 3
+
     if (options.city) {
       queryParams.push(`%${options.city}%`);
       queryString += `WHERE city LIKE $${queryParams.length} `;
@@ -131,7 +112,6 @@ const getAllProperties = function (options, limit = 10) {
       queryString += `WHERE cost_per_night > $${queryParams.length - 1} AND cost_per_night < $${queryParams.length}`;
     }
 
-    // 4
     queryString += `
     GROUP BY properties.id`
 
@@ -155,19 +135,8 @@ const getAllProperties = function (options, limit = 10) {
       }
     };
 
-
-    // // 5
-    console.log(queryString, queryParams);
-  
-    // 6
     return pool.query(queryString, queryParams)
     .then(res => res.rows);
-
-  // const limitedProperties = {};
-  // for (let i = 1; i <= limit; i++) {
-  //   limitedProperties[i] = properties[i];
-  // }
-  // return Promise.resolve(limitedProperties);
 };
 exports.getAllProperties = getAllProperties;
 
@@ -178,9 +147,10 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function (property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  const values = [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.street, property.city, property.province, property.post_code, property.country, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms]
+  const queryString = `INSERT INTO properties(owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING *;`
+  return pool.query(queryString, values)
+  .then( res => res.rows[0])
+  .catch(err => console.log('query error', err.stack))
 }
 exports.addProperty = addProperty;
